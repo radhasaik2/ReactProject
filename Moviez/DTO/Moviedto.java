5858c30917a5d8075281eb0b6e34e1eb329760f0
radhasaik2 <radhasaik.2@gmail.com>
2025-10-20 21:27:40 +0530

    moviez-tkt-booking Moviedto.java

---
+++ Moviez/DTO/Moviedto.java
@@ -0,0 +1,40 @@
+package in.movies.booking;
+
+public class Moviedto {
+	private int id;
+    private String title;
+    private String rating;
+    private String genre;
+    private String posterUrl;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getPosterUrl() {
+		return posterUrl;
+	}
+	public void setPosterUrl(String posterUrl) {
+		this.posterUrl = posterUrl;
+	}
+    
+}
