6172bbfe1860d42e7b01cda242a027245ef41105
radhasaik2 <radhasaik.2@gmail.com>
2025-10-20 21:21:34 +0530

    moviez-tkt-booking Logindto.java

---
+++ Moviez/DTO/Logindto.java
@@ -0,0 +1,26 @@
+package in.movies.booking;
+
+public class Logindto {
+	private String loginid;
+	private String email;
+	private String password;
+	public String getLoginid() {
+		return loginid;
+	}
+	public void setLoginid(String loginid) {
+		this.loginid = loginid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
